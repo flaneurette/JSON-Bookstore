3ec6e717bd661750b63a9b1e23edb77920105a85
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2019-04-10 17:14:30 +0200

    Update class.BookLibrary.php

--- class.BookLibrary.php
+++ class.BookLibrary.php
@@ -37,40 +37,6 @@ class BookLibrary {
 		return json_decode(file_get_contents(self::BOOKCASE), true, self::DEPTH, JSON_BIGINT_AS_STRING);
 	}
 	
-	// We don't use this, but you could call it to encrypt the JSON data.
-	public function encrypt($plaintext) {
-		
-		$key = self::PWD;
-		
-		$ivlen = openssl_cipher_iv_length($cipher="AES-256-CTR");
-		$iv = openssl_random_pseudo_bytes($ivlen);
-		$ciphertext_raw = openssl_encrypt($plaintext, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
-		$hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
-		$ciphertext = base64_encode($iv.$hmac.$ciphertext_raw );
-	
-		return bin2hex($ciphertext);
-	
-	}
-	
-	// We don't use this, but you could call it to decrypt the JSON data.
-	public function decrypt($ciphertext) {
-		
-		$key = self::PWD;
-		
-		$ciphertext = hex2bin($ciphertext);
-		$c = base64_decode($ciphertext);
-		$ivlen = openssl_cipher_iv_length($cipher="AES-256-CTR");
-		$iv = substr($c, 0, $ivlen);
-		$hmac = substr($c, $ivlen, $sha2len=32);
-		$ciphertext_raw = substr($c, $ivlen+$sha2len);
-		$original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
-		$calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
-		
-		if (hash_equals($hmac, $calcmac)) { //PHP 5.6+ timing attack safe comparison
-			return $original_plaintext;
-		}
-	}
-	
 	public function addBook() 
 	{
 		$newbook = 
@@ -205,6 +171,38 @@ class BookLibrary {
 		return function ($a, $b) use ($key) {
 			return strnatcmp($a[$key], $b[$key]);
 		};
+	}
+	
+	// We don't use this, but you could call it to encrypt the JSON data.
+	public function encrypt($plaintext) {
+		
+		$key = self::PWD; // Password is set above at the Constants
+		$ivlen = openssl_cipher_iv_length($cipher="AES-256-CTR");
+		$iv = openssl_random_pseudo_bytes($ivlen);
+		$ciphertext_raw = openssl_encrypt($plaintext, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+		$hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+		$ciphertext = base64_encode($iv.$hmac.$ciphertext_raw );
+	
+		return bin2hex($ciphertext);
+	
+	}
+	
+	// We don't use this, but you could call it to decrypt the JSON data.
+	public function decrypt($ciphertext) {
+		
+		$key = self::PWD; // Password is set above at the Constants
+		$ciphertext = hex2bin($ciphertext);
+		$c = base64_decode($ciphertext);
+		$ivlen = openssl_cipher_iv_length($cipher="AES-256-CTR");
+		$iv = substr($c, 0, $ivlen);
+		$hmac = substr($c, $ivlen, $sha2len=32);
+		$ciphertext_raw = substr($c, $ivlen+$sha2len);
+		$original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+		$calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+		
+		if (hash_equals($hmac, $calcmac)) { //PHP 5.6+ timing attack safe comparison
+			return $original_plaintext;
+		}
 	}
 }
 
